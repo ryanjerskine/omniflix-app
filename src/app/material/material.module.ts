6ebc2523887d09824ac7bf6d60a981d8b312bd06
ryanjerskine <ryan.j.erskine@gmail.com>
2019-12-08 03:22:02 -0600

    Adding progress bar.

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatProgressBarModule
   ],
   exports: [
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatProgressBarModule
   ]
 })
 export class MaterialModule { }
