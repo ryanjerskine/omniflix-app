16b1663fedb891817ea8a19d9efffe3c622853c3
ryanjerskine <ryan.j.erskine@gmail.com>
2019-12-09 03:00:54 -0600

    Adding animation and active indicator on links.

--- src/app/core/app/app.component.ts
+++ src/app/core/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { sideNavAnimation, sideNavContainerAnimation } from './sidenav.animations';
 
 @Component({
   selector: 'omni-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [sideNavAnimation, sideNavContainerAnimation]
 })
 export class AppComponent {
   isOpen = true;
   title = 'omniflix';
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
 }
